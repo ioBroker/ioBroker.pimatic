4d8ccc0263bc4230e5dad25786dc0faee5b7fd62
Ingo Fischer <github@fischer-ka.de>
2019-03-03 22:44:41 +0100

    add adapter-core

--- main.js
+++ main.js
@@ -10,7 +10,7 @@
 'use strict';
 
 // you have to require the utils module and call adapter function
-var utils   = require(__dirname + '/lib/utils'); // Get common adapter utils
+var utils = require('@iobroker/adapter-core'); // Get common adapter utils
 var request = require('request');
 var adapter = utils.Adapter('pimatic');
 var io      = require('socket.io-client');
